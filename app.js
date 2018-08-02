2a373eec1088f49c6b291fc0dbbad4fd84154c4d
Alfredo Bravo Cuero <abravo@iterasoft.co>
2018-08-02 02:16:57 -0500

    Cargando API - Fectch Api

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.getElementById('txtBtn').addEventListener('click', cargarTXT);
 
 document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 
+document.getElementById('apiBtn').addEventListener('click', cargarAPI);
+
 // Cargar Archivo TXT
 function cargarTXT() {
     fetch('datos.txt')
@@ -28,6 +30,32 @@ function cargarJSON() {
             data.forEach(function(empleado) {
                 html += `
                     <li>${empleado.nombre} ${empleado.empresa}</li>
+                `;
+            })
+
+            console.log(html);
+
+            document.getElementById('resultado').innerHTML = html;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+// Cargar Archivo API
+function cargarAPI() {
+    fetch('https://picsum.photos/list')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(imagenes) {
+            let html = '';
+            imagenes.forEach(function(imagen) {
+                html += `
+                    <li>
+                        <a target="_blank" href="${imagen.post_url}"> Ver Imagen</a>
+                        ${imagen.author}
+                    </li>
                 `;
             })
 
